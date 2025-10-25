2bb409d1443d3fb78852ab018b8ff2dfc292036b
DESKTOP-KV765JO\Jake <dawson9060@gmail.com>
2025-10-25 11:37:55 -0500

    added logo to header

--- src/components/SiteHeader.tsx
+++ src/components/SiteHeader.tsx
@@ -1,18 +1,26 @@
-import { Box, Group, Title } from '@mantine/core';
-import Link from 'next/link';
-import ContactModal from './ContactModal';
-import classes from './SiteHeader.module.css';
-import CalculatorModal from './CalculatorModal';
+import { Box, Group, Title } from '@mantine/core'
+import Link from 'next/link'
+import ContactModal from './ContactModal'
+import classes from './SiteHeader.module.css'
+import CalculatorModal from './CalculatorModal'
+import Image from 'next/image'
 
 const SiteHeader = () => {
   return (
     <nav className={classes.nav}>
       <Group className={classes.navRow}>
         <Link href="/" style={{ textDecoration: 'none' }}>
-          <Title order={3} c="black">Wissota Lumber</Title>
+          <Group gap="0">
+            <Image src="/LogoV2.png" width={50} height={50} alt="logo" />
+            <Title order={3} c="black">
+              Wissota Lumber
+            </Title>
+          </Group>
         </Link>
         <Group>
-          <Link href="/inventory" className={classes.links}>Inventory</Link>
+          <Link href="/inventory" className={classes.links}>
+            Inventory
+          </Link>
           <CalculatorModal />
           <ContactModal />
         </Group>
